b2003897b71695d62442a62afea767de1e02b002
iLLro <ispaspaul@yahoo.com>
2025-08-13 11:26:08 +0300

    main.tf creation

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "2.9.14"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url      = "https://192.168.111.100:8006/api2/json"
+  pm_user         = "root@pam"
+  pm_password     = "1qaz2wsx"
+  pm_tls_insecure = true
+}
+
+resource "proxmox_vm_qemu" "example" {
+  name        = "terraform-test"
+  target_node = "proxmox-node"
+  vmid        = 100
+  memory      = 1024
+  cores       = 1
+  sockets     = 1
+  disk {
+    size    = "10G"
+    type    = "scsi"
+    storage = "local-lvm"
+  }
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+  os_type = "cloud-init"
+  clone   = "template-name"
