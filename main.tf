28d4fb5733cdd24f2dc78fd247dabf4b4ed74f5e
iLLro <ispaspaul@yahoo.com>
2025-08-13 13:14:04 +0300

    all commits

--- main.tf
+++ main.tf
@@ -1,35 +1,85 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "Telmate/proxmox"
-      version = "2.9.14"
+      source = "bpg/proxmox"
+      version = "0.81.0"
     }
   }
 }
 
+
 provider "proxmox" {
-  pm_api_url      = "https://192.168.111.100:8006/api2/json"
-  pm_user         = "root@pam"
-  pm_password     = "1qaz2wsx"
-  pm_tls_insecure = true
+  endpoint = "https://192.168.111.100:8006/"
+  insecure = true
+  username = "root@pam"
+  password = "1qaz2wsx"
 }
 
-resource "proxmox_vm_qemu" "example" {
-  name        = "terraform-test"
-  target_node = "pve00"
-#  vmid        = 100
-  memory      = 1024
-  cores       = 1
-  sockets     = 1
-  disk {
-    size    = "10G"
-    type    = "scsi"
-    storage = "local-lvm"
+
+resource "proxmox_virtual_environment_vm" "ubuntu_template" {
+  name      = "ubuntu-template"
+  node_name = "pve00"
+
+  template = true
+  started  = false
+
+  machine     = "q35"
+  bios        = "ovmf"
+  description = "Managed by Terraform"
+
+  cpu {
+    cores = 2
   }
-  network {
-    model  = "virtio"
+
+  memory {
+    dedicated = 2048
+  }
+
+  efi_disk {
+    datastore_id = "pve-nfs"
+    type         = "4m"
+  }
+
+  disk {
+    datastore_id = "pve-nfs"
+    file_id      = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 20
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+
+    user_data_file_id = proxmox_virtual_environment_file.user_data_cloud_config.id
+  }
+
+  network_device {
     bridge = "vmbr0"
   }
-  os_type = "cloud-init"
-  clone   = "deb-templ"
-}  
+
+}
+
+resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = "pve00"
+  url = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
+}
+
+resource "proxmox_virtual_environment_file" "user_data_cloud_config" {
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = "pve00"
+
+  # The 'user-data.yaml' file should be placed in the root of your Terraform module directory.
+  # It must contain valid cloud-init user data for VM initialization.
+  source_file {
+    path = "${path.module}/user-data.yaml"
+  }
+}
